8f8c2694879f7788e0ac583a2b9e5c61e2b1f233
BoazLowenstein <odinux@gmail.com>
2025-01-19 16:25:24 +0200

    modified:   playground/ec2_instances.tf modified:   playground/ec2_w_user_data.tf chore update ec2 instances

--- playground/ec2_instances.tf
+++ playground/ec2_instances.tf
@@ -1,15 +1,15 @@
 ##ec2 instances we setup 
-resource "aws_spot_instance_request" "spot" {
-  ami           = data.aws_ami.ubuntu.id
-  spot_price = "0.003"
-  instance_type = var.instance_types.t3nano
-  subnet_id    = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids = [aws_security_group.all_ec2_rules.id]
-  key_name = aws_key_pair.odin.key_name
-  tags = {
-    Name = "ec2 spot instance"
-  }
-}
+#resource "aws_spot_instance_request" "spot" {
+#  ami           = data.aws_ami.ubuntu.id
+#  spot_price = "0.003"
+#  instance_type = var.instance_types.t3nano
+#  subnet_id    = element(module.vpc.public_subnets, 0)
+#  vpc_security_group_ids = [aws_security_group.all_ec2_rules.id]
+#  key_name = aws_key_pair.odin.key_name
+#  tags = {
+#    Name = "ec2 spot instance"
+#  }
+#}
 ##default user key location
 #resource "aws_key_pair" "odin" {
 #  key_name   = "odin"
--- playground/ec2_w_user_data.tf
+++ playground/ec2_w_user_data.tf
@@ -1,30 +1,30 @@
-resource "aws_instance" "user_data" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_types.t3nano
-
-  subnet_id    = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids = [aws_security_group.all_ec2_rules.id]
-
-  # the public SSH key
-  key_name = aws_key_pair.odin.key_name
-
-  # user data
-  user_data = data.cloudinit_config.cloudinit-example.rendered
-}
-
-resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "eu-west-2a"
-  size              = 20
-  type              = "gp2"
-  tags = {
-    Name = "extra volume data"
-  }
-}
-
-resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name  = var.INSTANCE_DEVICE_NAME
-  volume_id    = aws_ebs_volume.ebs-volume-1.id
-  instance_id  = aws_instance.user_data.id
-  skip_destroy = true                            # skip destroy to avoid issues with terraform destroy
-}
+#resource "aws_instance" "user_data" {
+#  ami           = data.aws_ami.ubuntu.id
+#  instance_type = var.instance_types.t3nano
+#
+#  subnet_id    = element(module.vpc.public_subnets, 0)
+#  vpc_security_group_ids = [aws_security_group.all_ec2_rules.id]
+#
+#  # the public SSH key
+#  key_name = aws_key_pair.odin.key_name
+#
+#  # user data
+#  user_data = data.cloudinit_config.cloudinit-example.rendered
+#}
+#
+#resource "aws_ebs_volume" "ebs-volume-1" {
+#  availability_zone = "eu-west-2a"
+#  size              = 20
+#  type              = "gp2"
+#  tags = {
+#    Name = "extra volume data"
+#  }
+#}
+#
+#resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+#  device_name  = var.INSTANCE_DEVICE_NAME
+#  volume_id    = aws_ebs_volume.ebs-volume-1.id
+#  instance_id  = aws_instance.user_data.id
+#  skip_destroy = true                            # skip destroy to avoid issues with terraform destroy
+#}
 
